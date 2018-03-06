3e2d1952e9a11e2d2ca74c4f42c073a7491d0857
Robert Buchholz <rbu@goodpoint.de>
2018-03-06 12:02:15 +0100

    Fix generating readme with python3

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ def readme():
     from subprocess import CalledProcessError
     try:
         from subprocess import check_output
-        return str(check_output(['pandoc', '--from', 'markdown', '--to', 'rst', 'README.md']))
+        return check_output(['pandoc', '--from', 'markdown', '--to', 'rst', 'README.md']).decode('utf-8')
     except (ImportError, OSError, CalledProcessError) as error:
-        print('python2.7 and pandoc is required to get the description as rst (as required to get nice rendering in pypi) - using the original markdown instead.',
+        print('pandoc is required to get the description as rst (as required to get nice rendering in pypi) - using the original markdown instead.',
               'See http://johnmacfarlane.net/pandoc/')
-    return str(open(path.join(here, 'Readme.md')).read())
+    return open(path.join(here, 'README.md')).read().decode('utf-8')
 
 
 setup(
